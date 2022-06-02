3051e85e530b0fed101664eaf1d1f70966487ca5
Gyula Németh <gyula@chamaileon.io>
2022-06-02 18:16:43 +0200

    feature: patchOne

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import list from './list.js'
 import createOne from './createOne.js'
 import readOne from './readOne.js'
 import updateOne from './updateOne.js'
+import patchOne from './patchOne.js'
 import deleteOne from './deleteOne.js'
 import deleteMany from './deleteMany.js'
 
@@ -10,6 +11,7 @@ export {
   createOne,
   readOne,
   updateOne,
+  patchOne,
   deleteOne,
   deleteMany
 }
---
+++ src/patchOne.js
@@ -0,0 +1,24 @@
+import NotFoundError from './errors/NotFoundError.js'
+
+export default async function updateOne (Model, params, body) {
+  const paramsCopy = Object.assign({}, params)
+  const _id = paramsCopy.id
+  delete paramsCopy.id
+
+  const filter = { _id, ...paramsCopy }
+  const originalItem = await Model.findOne(filter)
+  if (!originalItem) {
+    throw new NotFoundError(Model, filter)
+  }
+
+  const newData = { ...originalItem.toObject(), ...body, _id, ...paramsCopy }
+  const newItem = new Model(newData)
+  await newItem.validate()
+
+  const result = await Model.findByIdAndUpdate(_id, newData, { new: true })
+
+  return {
+    status: 200,
+    result: result.toObject()
+  }
+}
